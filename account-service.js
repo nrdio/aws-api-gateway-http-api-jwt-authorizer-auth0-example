1c0b9a7ff69cd62cc9c20474f150ff6b36a956b7
Nitin Deshmukh <nitin.rajen.deshmukh@accenture.com>
2020-06-17 22:22:38 +0200

    Added Account Service

---
+++ account-service.js
@@ -0,0 +1,52 @@
+const { AccountRepository } = require('./account-repository');
+
+const accountRepository = new AccountRepository();
+
+class AccountService {
+  async createAccount(accountRequest) {
+    try {
+      return accountRepository.createAccount(accountRequest);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  async getAccount(id) {
+    try {
+      return accountRepository.getAccount(id);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  async getAccounts() {
+    try {
+      return accountRepository.getAccounts();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  async updateAccount(id, accountUpdateRequest) {
+    try {
+      return accountRepository.updateAccount(id, accountUpdateRequest);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  async deleteAccount(id) {
+    try {
+      return accountRepository.deleteAccount(id);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+}
+
+exports.AccountService = AccountService;
